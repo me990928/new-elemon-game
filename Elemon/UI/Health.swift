09efab19b43b2eea793f1cc56b9434a5677c4973
me990928 <me990928@gmail.com>
2024-03-27 16:45:44 +0900

    add HealthUI

---
+++ Elemon/UI/Health.swift
@@ -0,0 +1,18 @@
+//
+//  Health.swift
+//  Elemon
+//
+//  Created by 広瀬友哉 on 2024/03/27.
+//
+
+import SwiftUI
+
+struct Health: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Health()
+}
