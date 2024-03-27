a8e19a208ea47040ff69c0749b0fb17c7840d131
me990928 <me990928@gmail.com>
2024-03-27 16:49:25 +0900

    add HitPoint ui

---
+++ Elemon/UI/HitPoint.swift
@@ -0,0 +1,16 @@
+//
+//  HitPoint.swift
+//  Elemon
+//
+//  Created by 広瀬友哉 on 2024/03/27.
+//
+
+import SwiftUI
+
+struct HitPoint: View {
+    @Binding var hitPoint: Double
+    @Binding var nowHitPoint: Double
+    var body: some View {
+        Text("HP: \(nowHitPoint) / \(hitPoint)")
+    }
+}
