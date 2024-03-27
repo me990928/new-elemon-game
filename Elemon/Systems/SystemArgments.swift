0291511e1b44a7ebb850fd154db2e817805cc8ee
me990928 <me990928@gmail.com>
2024-03-28 02:46:43 +0900

    create SystemArgments model

---
+++ Elemon/Systems/SystemArgments.swift
@@ -0,0 +1,15 @@
+//
+//  SystemArgments.swift
+//  Elemon
+//
+//  Created by 広瀬友哉 on 2024/03/28.
+//
+
+import Foundation
+import SwiftUI
+import SwiftData
+
+@Model
+final class SystemArgments {
+    
+}
