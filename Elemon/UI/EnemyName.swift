4cf0e22d3bc690024512e840fc142d5395d07b4c
me990928 <me990928@gmail.com>
2024-03-27 16:35:33 +0900

    Add show nameUI

---
+++ Elemon/UI/EnemyName.swift
@@ -0,0 +1,18 @@
+//
+//  EnemyName.swift
+//  Elemon
+//
+//  Created by 広瀬友哉 on 2024/03/27.
+//
+
+import SwiftUI
+
+struct EnemyName: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnemyName()
+}
